30a39e4852b94d138ed989e6610ee6d9074e67b5
LachlanS9 <131934212+LachlanS9@users.noreply.github.com>
2026-08-08 23:48:01 +1000

    Fill in real SHA256 checksums for the v0.1.0 release binaries

--- Formula/codegraph.rb
+++ Formula/codegraph.rb
@@ -6,7 +6,7 @@ class Codegraph < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/LachlanS9/codegraph/releases/download/v#{version}/codegraph-macos-arm64"
-      sha256 "PLACEHOLDER_MACOS_ARM64_SHA256"
+      sha256 "61dca77eda4c80570f016b7aefae16dad145880e5daf3a207ebc421047344677"
     else
       odie "codegraph does not currently ship an Intel Mac (x86_64) binary."
     end
@@ -15,10 +15,10 @@ class Codegraph < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/LachlanS9/codegraph/releases/download/v#{version}/codegraph-linux-arm64"
-      sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
+      sha256 "ebfec837afbe356b4fa07461d7f6c7fbb42d5a28005a4c577524d7da4fb8116a"
     else
       url "https://github.com/LachlanS9/codegraph/releases/download/v#{version}/codegraph-linux-x64"
-      sha256 "PLACEHOLDER_LINUX_X64_SHA256"
+      sha256 "8c6e760f784622a1b1e0c20958bf6a5679dc2bc0d27cfb4dedb11dc3c3236a87"
     end
   end
 
